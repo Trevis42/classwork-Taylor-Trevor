bfa2faff6a4b1c9d0c52312321d85acd7f2dade5
Trevis42 <sirtrevis42@gmail.com>
2020-11-04 11:23:44 -0600

    initial commit

---
+++ obstructiongame/src/main/java/com/talentpath/obstructiongame/ObstructiongameApplication.java
@@ -0,0 +1,13 @@
+package com.talentpath.obstructiongame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObstructiongameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObstructiongameApplication.class, args);
+	}
+
+}
